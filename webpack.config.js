f1cf6cd76445ba7e1af6a598b05399645d2ecfdb
Oleksandr Telnov <oleksandr.telnov@valor-software.com>
2016-10-05 11:40:02 +0300

    add npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
+/* eslint no-process-env: 0, global-require:0 */
 'use strict';
 
 module.exports = getWebpackConfig(process.env.NODE_ENV, require('./.ng2-config'));
 
 function getWebpackConfig(env, config) {
   switch (env) {
-    case 'prod':
-    case 'production':
-      return require('ng2-webpack-config').webpack.prod(config);
-    case 'test':
-    case 'testing':
-      return require('ng2-webpack-config').webpack.test(config);
-    case 'dev':
-    case 'development':
-    default:
-      return require('ng2-webpack-config').webpack.dev(config);
+  case 'prod':
+  case 'production':
+    return require('ng2-webpack-config').webpack.prod(config);
+  case 'test':
+  case 'testing':
+    return require('ng2-webpack-config').webpack.test(config);
+  case 'dev':
+  case 'development':
+  default:
+    return require('ng2-webpack-config').webpack.dev(config);
   }
 }
