80f8643a53d4b4f81aff0b125d29c03954ef9c7a
Oleksandr Telnov <oleksandr.telnov@valor-software.com>
2016-10-10 16:49:36 +0300

    hotfix(particlesjs): just hide top block instead of ngIf to prevent particlesjs block will be not exists in dom

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-// require('./assets/css/bootstrap.min.css');
-// require('./assets/css/styles.css');
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './app.module';
