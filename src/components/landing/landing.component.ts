9fe08167b40bfe7b4b3736a0225b81be3ee45d9e
Oleksandr Telnov <oleksandr.telnov@valor-software.com>
2016-10-11 13:28:23 +0300

    feat(feedback): move feedback component to partials

--- src/components/landing/landing.component.ts
+++ src/components/landing/landing.component.ts
@@ -6,22 +6,5 @@ require('./landing.css');
   template: require('./landing.html')
 })
 export class LandingComponent {
-  public myInterval:number = 500000;
-  public noWrapSlides:boolean = false;
-  public slides:Array<any> = [];
   public getImage = (img:string):string => require('./images/' + img);
-
-  public constructor() {
-    for (let i = 0; i < 4; i++) {
-      this.addSlide();
-    }
-  }
-
-  public addSlide():void {
-    let newWidth = 600 + this.slides.length + 1;
-    this.slides.push({
-      image: `//placekitten.com/${newWidth}/300`,
-      text: `${['1', '2', '3', '4'][this.slides.length % 4]}`
-    });
-  }
 }
