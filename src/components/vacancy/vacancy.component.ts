f8ec9a17bbbf4bd5e2553fc1aa4393278074542a
daniloff200 <daniloff200@gmail.com>
2016-10-13 14:26:22 +0300

    fix(vacancy): create new component for each vacancy, move form here

--- src/components/vacancy/vacancy.component.ts
+++ src/components/vacancy/vacancy.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'vacancy',
   template: require('./vacancy.html')
 })
-export class VacancyComponent {
+export class VacancyComponent implements OnInit {
+  public pageName: string;
 
+  public ngOnInit(): void {
+
+    this.pageName = 'Vacancy page';
+  }
 }
