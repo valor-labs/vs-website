5d402a6d24593a76d655ebd24b2795c589542aa5
daniloff200 <daniloff200@gmail.com>
2016-10-17 14:42:47 +0300

    feat(contact.page): add block 'let's talk' and social networks

--- src/components/contact/contact.component.ts
+++ src/components/contact/contact.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+require('./contact.css');
 @Component({
   selector: 'contact',
-  // styles: [require('./contact.css')],
   template: require('./contact.html')
 })
 export class ContactComponent implements OnInit {
   public pageName: string;
+  public content: Array<any> = [];
+  public email: any = {};
+  public location: any = {};
 
   public ngOnInit(): void {
 
+    this.email.title = 'Contact e-mail:';
+    this.email.text = 'hello@valor-software.com';
+    this.location.title = 'Location:';
+    this.location.text = 'Ukraine, Kharkiv';
     this.pageName = 'Contact page';
+
+    this.content.push(this.email);
+    this.content.push(this.location);
 
   }
 
