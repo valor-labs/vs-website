e581bf6aa45f83653f669e5cb80cdd4a419c9b63
Oleksandr Telnov <oleksandr.telnov@valor-software.com>
2016-10-18 18:17:33 +0300

    chore(travis): fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SHA=`git rev-parse --verify HEAD`
-URL='git@github.com:VS-work/VS-work.github.io.git'
+branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+echo $branch
 
-openssl aes-256-cbc -K $encrypted_3fc7c455333c_key -iv $encrypted_3fc7c455333c_iv -in deploy_key.enc -out deploy_key -d
+if [ "$branch" != "development" ]
+then
+  exit 0
+else
+  echo "deploying to gh-pages..."
+  SHA=`git rev-parse --verify HEAD`
+  URL='git@github.com:VS-work/VS-work.github.io.git'
 
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
+  openssl aes-256-cbc -K $encrypted_3fc7c455333c_key -iv $encrypted_3fc7c455333c_iv -in deploy_key.enc -out deploy_key -d
 
-./node_modules/.bin/gh-pages -d dist -r "${URL}" -b master -m "Deploy to GitHub Pages: ${SHA}"
+  chmod 600 deploy_key
+  eval `ssh-agent -s`
+  echo -ne '\n' | ssh-add deploy_key
+
+  ./node_modules/.bin/gh-pages -d dist -r "${URL}" -b master -m "Deploy to GitHub Pages: ${SHA}"
+fi
