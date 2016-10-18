726fcc90bc4c30149343a6f117e884f041b4fa3e
Oleksandr Telnov <oleksandr.telnov@valor-software.com>
2016-10-18 17:03:06 +0300

    chore(travis): add deploy_key

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+SHA=`git rev-parse --verify HEAD`
+URL='git@github.com:VS-work/VS-work.github.io.git'
+
+openssl aes-256-cbc -K $encrypted_3fc7c455333c_key -iv $encrypted_3fc7c455333c_iv -in deploy_key.enc -out deploy_key -d
+
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+./node_modules/.bin/gh-pages -d dist -r "${URL}" -b master -m "Deploy to GitHub Pages: ${SHA}"
