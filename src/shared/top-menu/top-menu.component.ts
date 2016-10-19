5ac50582800fee9d0dd9ebff189afc1089eac624
Denis <denisvad1313@rambler.ru>
2016-10-19 13:48:17 +0300

    mobile version

--- src/shared/top-menu/top-menu.component.ts
+++ src/shared/top-menu/top-menu.component.ts
@@ -7,8 +7,19 @@ require('./top-menu.css');
 })
 export class TopMenuComponent implements OnInit {
   public isSticky:boolean = false;
+  public isOpen: boolean = false;
   public startFixedPosition:number = window.innerHeight;
   @Input() public isLanding:boolean;
+
+  public menuToggle():void {
+    this.isOpen = !this.isOpen;
+
+    if (this.isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }
 
   public ngOnInit():void {
     require('./particles-config');
