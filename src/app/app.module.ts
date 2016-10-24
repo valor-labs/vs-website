359973dbe5b3d1e7843af0d9e3d7388132e587f2
Svyat <svyat.plehanov@gmail.com>
2016-10-24 14:11:14 +0300

    fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,101 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { CarouselModule } from 'ng2-bootstrap/components/carousel';
+import { Ng2PageScrollModule } from '../vendors/ng2-page-scroll/ng2-page-scroll';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from '../components/main/main.component';
+
+import { TopMenuComponent } from '../shared/top-menu/top-menu.component';
+import { HamburgerMenuComponent } from '../shared/hamburger-menu/hamburger-menu.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+
+import { LandingComponent } from '../components/landing/landing.component';
+import { ApproachComponent } from '../components/approach/approach.component';
+import { ServicesComponent } from '../components/services/services.component';
+import { ProjectsComponent } from '../components/projects/projects.component';
+import { ProjectComponent } from '../components/project/project.component';
+import { AboutComponent } from '../components/about/about.component';
+import { TeamComponent, MemberComponent } from '../components/team/team.component';
+import { CareersComponent } from '../components/careers/careers.component';
+import { ContactComponent } from '../components/contact/contact.component';
+import { VacancyComponent } from '../components/vacancy/vacancy.component';
+import { FormsComponent } from '../partials/forms/forms.component';
+
+import { ProjectPreviewComponent, ProjectsListComponent } from '../partials/projects-list/projects-list.component';
+import { FeedbackComponent } from '../partials/feedback/feedback.component';
+import { SingleContentComponent } from '../partials/single-content/single-content.component';
+import { DuplexContentComponent } from '../partials/duplex-content/duplex-content.component';
+import { SectionHeadingComponent } from '../partials/section-heading/section-heading.component';
+import { HeaderComponent } from '../partials/header/header.component';
+
+import { VacanciesListComponent, VacancyPreviewComponent } from '../partials/vacancies-list/vacancies-list.component';
+
+import { MainService } from '../services/main.service';
+import { ProjectsService } from '../services/projects.service';
+import { VacanciesService } from '../services/vacancies.service';
+import { ContentService } from '../services/content.service';
+import { ConfigProvider } from '../services/config.service';
+
+import { RouterModule } from '@angular/router';
+import { routes } from '../router.config';
+
+const routing = RouterModule.forRoot(routes);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuComponent,
+    HamburgerMenuComponent,
+    FooterComponent,
+    MainComponent,
+
+    LandingComponent,
+    ApproachComponent,
+    ServicesComponent,
+    ProjectsComponent,
+    ProjectComponent,
+    AboutComponent,
+    TeamComponent,
+    FormsComponent,
+    CareersComponent,
+    ContactComponent,
+
+    MemberComponent,
+    ProjectPreviewComponent,
+    ProjectsListComponent,
+    SingleContentComponent,
+    DuplexContentComponent,
+    SectionHeadingComponent,
+    FeedbackComponent,
+    HeaderComponent,
+    VacanciesListComponent,
+    VacancyComponent,
+    VacancyPreviewComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing,
+    Ng2PageScrollModule,
+    CarouselModule
   ],
-  providers: [],
+
+  providers: [
+    MainService,
+    ConfigProvider,
+    ProjectsService,
+    ContentService,
+    VacanciesService,
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+  ],
+
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/';
 
 if (ENV === 'production') {
   enableProdMode();
